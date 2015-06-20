618c5f59cf8f2fd3b84720764c5caa32941e4899
hqythu <hqythu@gmail.com>
2015-06-20 16:25:31 +0800

    can now display delauny triangulation

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,65 @@
+#include <ctime>
 #include <iostream>
-#include <CGAL/Simple_cartesian.h>
-typedef CGAL::Simple_cartesian<double> Kernel;
-typedef Kernel::Point_2 Point_2;
-typedef Kernel::Segment_2 Segment_2;
-int main()
-{
-    Point_2 p(1,1), q(10,10);
-    std::cout << "p = " << p << std::endl;
-    std::cout << "q = " << q.x() << " " << q.y() << std::endl;
-    std::cout << "sqdist(p,q) = "
-    << CGAL::squared_distance(p,q) << std::endl;
+#include <fstream>
+#include <vector>
+#include <random>
+#include <chrono>
 
-    Segment_2 s(p,q);
-    Point_2 m(5, 9);
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
-    std::cout << "m = " << m << std::endl;
-    std::cout << "sqdist(Segment_2(p,q), m) = "
-    << CGAL::squared_distance(s,m) << std::endl;
-    std::cout << "p, q, and m ";
-    switch (CGAL::orientation(p,q,m)){
-        case CGAL::COLLINEAR:
-            std::cout << "are collinear\n";
-            break;
-        case CGAL::LEFT_TURN:
-            std::cout << "make a left turn\n";
-            break;
-        case CGAL::RIGHT_TURN:
-            std::cout << "make a right turn\n";
-            break;
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Delaunay_triangulation_2.h>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::ifstream;
+using std::ofstream;
+using std::vector;
+
+typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
+typedef CGAL::Delaunay_triangulation_2<K> Delaunay;
+typedef K::Point_2 Point;
+
+std::default_random_engine generator(clock());
+std::uniform_int_distribution<int> uniform(0, 600);
+
+int main() {
+    vector<Point> points;
+    for (int i = 0; i < 10; i++) {
+        int x = uniform(generator), y = uniform(generator);
+        cout << x << " " << y << endl;
+        points.push_back(Point(x, y));
     }
-    std::cout << " midpoint(p,q) = " << CGAL::midpoint(p,q) << std::endl;
+
+    Delaunay dt;
+    dt.insert(points.begin(), points.end());
+
+    int width = 600, height = 600;
+    cv::Mat picture(height, width, CV_8UC3, cv::Scalar(255, 255, 255));
+
+    for (auto it = dt.finite_faces_begin(); it != dt.finite_faces_end(); ++it) {
+        auto v1 = it->vertex(0)->point();
+        auto v2 = it->vertex(1)->point();
+        auto v3 = it->vertex(2)->point();
+        cv::line(picture,
+                cv::Point(v1.hx(), v1.hy()),
+                cv::Point(v2.hx(), v2.hy()),
+                cv::Scalar(0, 0, 0));
+        cv::line(picture,
+                 cv::Point(v2.hx(), v2.hy()),
+                 cv::Point(v3.hx(), v3.hy()),
+                 cv::Scalar(0, 0, 0));
+        cv::line(picture,
+                 cv::Point(v3.hx(), v3.hy()),
+                 cv::Point(v1.hx(), v1.hy()),
+                 cv::Scalar(0, 0, 0));
+    }
+
+    cv::namedWindow("hqy");
+    cv::imshow("hqy", picture);
+    cv::waitKey(0);
+
     return 0;
 }
